dafc743db9c0d88d9c0c9c9d729fb6bfc9f81694
RAM <ram.tamang2058@gmail.com>
2022-11-12 07:59:36 +0545

    Updates

---
+++ precache-manifest.dac037e5b3a822eb2d627572e38a5b8b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b0dbd82087e4bad0ed",
+    "url": "/static/css/main.f7fda37e.chunk.css"
+  },
+  {
+    "revision": "35b0dbd82087e4bad0ed",
+    "url": "/static/js/main.35b0dbd8.chunk.js"
+  },
+  {
+    "revision": "9240f9f763df1a6e54e0",
+    "url": "/static/js/1.9240f9f7.chunk.js"
+  },
+  {
+    "revision": "ed1cbb2658c8d5418da9",
+    "url": "/static/css/2.c887ade2.chunk.css"
+  },
+  {
+    "revision": "ed1cbb2658c8d5418da9",
+    "url": "/static/js/2.ed1cbb26.chunk.js"
+  },
+  {
+    "revision": "efeec7a8e0c00033fdbe",
+    "url": "/static/js/3.efeec7a8.chunk.js"
+  },
+  {
+    "revision": "036ce34a9e17e4682b98",
+    "url": "/static/js/runtime~main.036ce34a.js"
+  },
+  {
+    "revision": "bc47682f41da42a5d7ce82417125302d",
+    "url": "/static/media/CV_OF_RAM_KUMAR_TAMANG.bc47682f.pdf"
+  },
+  {
+    "revision": "1aed08b3e1b66991d921d25dbdcf28ed",
+    "url": "/static/media/home-main.1aed08b3.svg"
+  },
+  {
+    "revision": "3c9241c2d284b85346b0c8e719568989",
+    "url": "/static/media/avatar.3c9241c2.svg"
+  },
+  {
+    "revision": "cf8c6935f68fde1543684d4a817f213a",
+    "url": "/static/media/pre.cf8c6935.svg"
+  },
+  {
+    "revision": "bc2c7afa280d7a7d54e2c5ecf515e8a6",
+    "url": "/static/media/home-bg.dc52d5d4.jpg"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/static/media/about.aee0f771.png"
+  },
+  {
+    "revision": "c512d9da504be4d96313504211775d4e",
+    "url": "/static/media/leaf.ff2616f6.png"
+  },
+  {
+    "revision": "750c7d884d31442e823bce13781275b9",
+    "url": "/static/media/emotion.eabfc16e.png"
+  },
+  {
+    "revision": "408d49ba10562bd34236c63d4e6af221",
+    "url": "/static/media/codeEditor.1b585492.png"
+  },
+  {
+    "revision": "44190220743b70a49855d73399eec8d8",
+    "url": "/static/media/chatify.7206044e.png"
+  },
+  {
+    "revision": "22cb85719800191ea12a9361fbfef9b2",
+    "url": "/static/media/suicide.9c86cd71.png"
+  },
+  {
+    "revision": "69948a28ccbd01fdd576340358088e37",
+    "url": "/static/media/blog.fb8ab7f2.png"
+  },
+  {
+    "revision": "6e56c1f2576902741404cd5d91b9fa88",
+    "url": "/index.html"
+  }
+];
